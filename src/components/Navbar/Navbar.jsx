bb7f2f73d7a393eeb91350a7a1dde509451ea840
Vipin Kumar <vipintech195504@gmail.com>
2024-08-26 16:11:38 +0530

    add responsiveness for the mobile device

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,43 +4,46 @@ import { AiOutlineHome } from "react-icons/ai";
 import { PiUserCircle } from "react-icons/pi";
 import { IoLayersOutline } from "react-icons/io5";
 import { VscGithubProject } from "react-icons/vsc";
-import { RiBloggerLine } from "react-icons/ri";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { RiContactsLine } from "react-icons/ri";
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
-  return (
-    <div className='nav-border'>
-        <div className='nav-item'>
-            <Link to={'/'} className='logo-container'>
-                <img className='logo-size' src={Logo} />
-            </Link>
-            <div className='nav-container'>
-                <Link to={'/'} className='nav show-active'  >
-                    <AiOutlineHome className='icon'/>
-                    <p>Home</p>
+    return (
+        <div className='nav-border'>
+            <div className='nav-item'>
+                <Link to={'/'} className='logo-container'>
+                    <img className='logo-size' src={Logo} />
                 </Link>
-                <Link  to={'/about'} className='nav '>
-                    <PiUserCircle className='icon'/>
-                    <p>About</p>
+                <div className='nav-container'>
+                    <Link to={'/'} className='nav show-active'  >
+                        <AiOutlineHome className='icon' />
+                        <p>Home</p>
+                    </Link>
+                    <Link to={'/about'} className='nav '>
+                        <PiUserCircle className='icon' />
+                        <p>About</p>
+                    </Link>
+                    <Link to={'/services'} className='nav' >
+                        <IoLayersOutline className='icon' />
+                        <p>Services</p>
+                    </Link>
+                    <Link to={'/project'} className='nav' >
+                        <VscGithubProject className='icon' />
+                        <p>Project</p>
+                    </Link>
+                    <Link to={'/contact'} className='nav'>
+                        <RiContactsLine className='icon' />
+                        <p>Contact</p>
+                    </Link>
+                </div>
+                <Link to={'/contact'}>
+                    <button className='lets-talk'>Let's Talks</button>
                 </Link>
-                <Link to={'/services'} className='nav' >
-                    <IoLayersOutline className='icon'/>
-                    <p>Services</p>
-                </Link>
-                <Link to={'/project'} className='nav' >
-                    <VscGithubProject className='icon'/>
-                    <p>Project</p>
-                </Link>
-                <Link to={'/contact'} className='nav'>
-                    <RiContactsLine className='icon'/>
-                    <p>Contact</p>
-                </Link>
+                <div className='hamburger-menu-bar'>
+                    <GiHamburgerMenu/>
+                </div>
             </div>
-            <Link to={'/contact'}>
-                <button className='lets-talk'>Let's Talks</button>
-            </Link>
         </div>
-     </div>
     )
 }
 export default Navbar
